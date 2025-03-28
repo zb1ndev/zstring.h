e8dd56ae142476119952523fb7f14c8aa6fed6b4
zb1ndev <zbindev@gmail.com>
2025-03-27 20:22:37 -0700

    License Added

--- zstring.h
+++ zstring.h
@@ -1,4 +1,27 @@
-/* zstring.h - v1.0.0 - Public Domain - https://github.com/zb1ndev/zstring.h */
+/* zstring.h - v1.0.0 - MIT License - https://github.com/zb1ndev/zstring.h 
+
+    MIT License
+    Copyright (c) 2025 Joel Zbinden
+
+    Permission is hereby granted, free of charge, to any person obtaining a copy
+    of this software and associated documentation files (the "Software"), to deal
+    in the Software without restriction, including without limitation the rights
+    to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+    copies of the Software, and to permit persons to whom the Software is
+    furnished to do so, subject to the following conditions:
+
+    The above copyright notice and this permission notice shall be included in all
+    copies or substantial portions of the Software.
+
+    THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+    IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+    FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+    AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+    LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+    OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
+    SOFTWARE.
+
+*/
 
 #if !defined(STRING_H)
 #define STRING_H
